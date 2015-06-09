67045a457ec451cd3586fe7ca28f139eb0b2cc97
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2015-06-09 15:40:09 +0200

    Rework OrderPaymentController to update the order state on bank payment response

--- Controller/Payments/OrderPaymentController.php
+++ Controller/Payments/OrderPaymentController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tms\Bundle\PaymentBundle\Model\Payment;
 
 /**
  * Order Payment controller.
@@ -24,62 +25,46 @@ class OrderPaymentController extends Controller
     public function autoResponseAction(Request $request, $order_id, $backend_alias)
     {
         $requestData = $request->isMethod('POST') ? $request->request : $request->query;
-
         $logger = $this->container->get('logger');
         $logger->info(sprintf('Payment server "%s" response: %s',
             $backend_alias,
             json_encode($requestData->all())
         ));
 
-        $paymentBackend = $this
-            ->container
-            ->get('tms_payment.backend_registry')
-            ->getBackend($alias)
-        ;
-/*
-        $crawler = $this
-            ->container
-            ->get('tms_rest_client.hypermedia.crawler')
+        $paymentBackend = $this->container->get('tms_payment.backend_registry')
+            ->getBackend($backend_alias)
         ;
 
-        $order = $crawler
-            ->go('order')
-            ->findOne('/orders', $results['order_id'])
-            ->getData()
-        ;
+        $crawler = $this->container->get('tms_rest_client.hypermedia.crawler');
+        $order = $crawler->go('order')->findOne('/orders', $order_id)->getData();
 
-        if ($order['processingState'] == 'N') {
+        if ('Q' != $order['processingState']) {
+            $logger->error(sprintf('The order %s must be at the state "Q" and not "%s"',
+                $order_id,
+                $order['processingState']
+            ));
+
             return new Response();
         }
 
-        $patchOrder = array(
-            'payment' => array_merge(
-                isset($order['payment']) ? $order['payment'] : array(),
-                array(
-                    'type' => $name,
-                    'raw'  => $results,
-                )
-            )
-        );
+        $payment = $paymentBackend->getPayment($request, $order['payment']);
+        $patchOrder = array('payment' => $payment->toArray());
 
-        if ($paymentBackend->isValidPayment($results)) {
-            $patchOrder['payment']['status'] = 'success';
+        if ($payment->getState() == Payment::STATE_APPROVED) {
             $patchOrder['processingState'] = 'N';
             $now = new \DateTime();
             $patchOrder['confirmedAt'] = $now->format(\DateTime::ISO8601);
-        } else {
-            $patchOrder['payment']['status'] = 'error';
         }
 
         $crawler
             ->go('order')
             ->execute(
-                sprintf('/orders/%s', $results['order_id']),
+                sprintf('/orders/%s', $order_id),
                 'PATCH',
                 $patchOrder
             )
         ;
-*/
+
         return new Response();
     }
 }
